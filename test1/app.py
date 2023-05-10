02380eb35acefe67bfa793128acbcf0b950c0d48
Heibert <heibertvts1997@gmail.com>
2023-05-10 17:54:17 -0500

    First test of Python code

--- test1/app.py
+++ test1/app.py
@@ -1,7 +1,41 @@
+# VARIABLES
+name = "Jose"
+age = 26
+isAdmin = True
+
+print(f"{name} has {str(age)} years and that is equal to {isAdmin}")
+
+# COLLECTION
+
 UserCollection = {
     "name": "Joseph",
     "age": 26,
     "genere": "Male"
 }
 
-print(UserCollection);
+print(UserCollection)
+
+# FUNCTION
+def sumar(num1, num2):
+    if(not num1) or (not num2):
+        return "Numbers are required!"
+    else:
+        return num1 + num2
+    
+result = sumar(5, 10)
+print(result)
+
+# REQUESTS
+import requests
+
+def getPost():
+    url_jsonplaceholder = 'https://jsonplaceholder.typicode.com/posts/1'
+    response = requests.get(url_jsonplaceholder)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else: 
+        return 'Error request'
+
+print(getPost())
